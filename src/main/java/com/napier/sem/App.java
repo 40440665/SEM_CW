b8c3104566c0474a9c8291a4cfbe44b015c66263
Euan <40440665@live.napier.ac.uk>
2022-03-22 18:35:19 +0000

    Added get all cities in a country

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -26,8 +26,15 @@ public class App {
         //Display cities by district
         //ArrayList<City> cities = Test.citiesByDistrict();
 
+        //Display cities in a Region
+        //ArrayList<City> cities = Test.getAllCitiesInARegion();
+
+        //Display cities in a Country
+        ArrayList<City> cities = Test.getAllCitiesInACountry();
+
+
         //Display top N populated cities
-        ArrayList<City> cities = Test.NPopulatedCities();
+        //ArrayList<City> cities = Test.NPopulatedCities();
 
         //Print the cities
         Test.printCities(cities);
@@ -56,11 +63,11 @@ public class App {
                 try {
                     // Wait a bit for db to start
                     //Changing to '0' allows for fast connection with localhost:33060 [Originally 30000]
-                    Thread.sleep(0);
+                    Thread.sleep(30000);
 
                     // Connect to database
                     // "localhost:33060" Makes a fast connection to the database. [Originally db:3306]
-                    con = DriverManager.getConnection("jdbc:mysql://localhost:33060/world?useSSL=false", "root", "example");
+                    con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                     System.out.println("Successfully connected");
                     // Wait a bit
                     Thread.sleep(10000);
@@ -241,5 +248,69 @@ public class App {
             return null;
         }
     }
+
+    public ArrayList<City> getAllCitiesInARegion() {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT city.Name, city.CountryCode, city.District, city.Population "
+                            + "FROM city, country "
+                            + "WHERE city.CountryCode = country.Code "
+                            + "AND country.Region = 'Caribbean' ";
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<City> cities = new ArrayList<City>();
+            while (rset.next()) {
+                City city = new City();
+                city.Name = rset.getString("city.Name");
+                city.CountryCode = rset.getString("city.CountryCode");
+                city.District = rset.getString("city.District");
+                city.Population = rset.getInt("city.Population");
+                cities.add(city);
+            }
+            return cities;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+    }
+
+
+    public ArrayList<City> getAllCitiesInACountry() {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT city.Name, city.CountryCode, city.District, city.Population "
+                            + "FROM city, country "
+                            + "WHERE city.CountryCode = country.Code "
+                            + "AND country.Name = 'France' ";
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<City> cities = new ArrayList<City>();
+            while (rset.next()) {
+                City city = new City();
+                city.Name = rset.getString("city.Name");
+                city.CountryCode = rset.getString("city.CountryCode");
+                city.District = rset.getString("city.District");
+                city.Population = rset.getInt("city.Population");
+                cities.add(city);
+            }
+            return cities;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+    }
+
 
 }
