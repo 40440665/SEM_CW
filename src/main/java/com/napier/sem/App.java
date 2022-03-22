649d061d1f50554dfb449b7084a2605041ddfedb
Artemiy Matusevs <40485274@live.napier.ac.uk>
2022-03-22 15:07:51 +0000

    Clean up main function

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -3,45 +3,66 @@ package com.napier.sem;
 import java.sql.*;
 
 public class App {
+    Connection con = null;
+
     public static void main(String[] args) {
-        try {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
+        //Create new application
+        App Test = new App();
+
+        //Connect to database
+        Test.connect();
+
+        //Disconnect from database
+        Test.disconnect();
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
-        for (int i = 0; i < retries; ++i) {
-            System.out.println("Connecting to database...");
+
+        //Connect to the database
+        public void connect()
+        {
             try {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
-                break;
-            } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                System.out.println(sqle.getMessage());
-            } catch (InterruptedException ie) {
-                System.out.println("Thread interrupted? Should not happen.");
+                // Load Database driver
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                System.out.println("Could not load SQL driver");
+                System.exit(-1);
+            }
+
+            // Connection to the database
+            int retries = 100;
+            for (int i = 0; i < retries; ++i) {
+                System.out.println("Connecting to database...");
+                try {
+                    // Wait a bit for db to start
+                    Thread.sleep(30000);
+                    // Connect to database
+                    con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                    System.out.println("Successfully connected");
+                    // Wait a bit
+                    Thread.sleep(10000);
+                    // Exit for loop
+                    break;
+                } catch (SQLException sqle) {
+                    System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                    System.out.println(sqle.getMessage());
+                } catch (InterruptedException ie) {
+                    System.out.println("Thread interrupted? Should not happen.");
+                }
             }
         }
 
-        if (con != null) {
-            try {
-                // Close connection
-                con.close();
-            } catch (Exception e) {
-                System.out.println("Error closing connection to database");
+        //Disconnect from the database
+        public void disconnect()
+        {
+            if (con != null) {
+                try {
+                    // Close connection
+                    con.close();
+                    System.out.println("Dsiconnecting from database");
+                } catch (Exception e) {
+                    System.out.println("Error closing connection to database");
+                }
             }
         }
+
     }
-}
---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,21 @@
+package com.napier.sem;
+
+
+//Represents a city
+public class City {
+
+    //City ID
+    public int ID;
+
+    //City Name
+    public String Name;
+
+    //Country Code
+    public String CountryCode;
+
+    //District
+    public String District;
+
+    //Population
+    public String Population;
+}
