7ea056927c2bcef3a6ef49ee1288db94fb25970d
Euan <40440665@live.napier.ac.uk>
2022-03-23 08:34:46 +0000

    Added Integration tests

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -12,14 +12,14 @@ public class AppIntegrationTest
     @BeforeAll
     static void init()
     {
-        //Create new application
         app = new App();
-
-        //Connect to database
         app.connect("localhost:33060", 30000);
 
     }
 
+
+
+    ///////////////////////////// CHANGE THESE TEST VALUES!!!!!!!!!!!!!!!!!!
     @Test
     void testCitiesByPopulation()
     {
@@ -30,6 +30,9 @@ public class AppIntegrationTest
         assertEquals(cities.get(0).Population , 3694820);
     }
 
+
+
+    ///////////////////////////// CHANGE THESE TEST VALUES!!!!!!!!!!!!!!!!!!
     @Test
     void testGetAllCitiesInAContinent()
     {
@@ -50,13 +53,96 @@ public class AppIntegrationTest
         assertEquals(cities.get(0).Population , 3694820);
     }
 
+
+
+
+    ///////////////////////////// CHANGE THESE TEST VALUES!!!!!!!!!!!!!!!!!!
+
     @Test
-    void testNPopulatedCitiesInADistrict()
+    void testCitiesInARegion()
     {
-        ArrayList<City> cities = app.NPopulatedCitiesInADistrict(1,"California");
+        ArrayList<City> cities = app.getAllCitiesInARegion("Caribbean");
+        assertEquals(cities.get(0).Name, "Los Angeles");
+        assertEquals(cities.get(0).CountryCode, "USA");
+        assertEquals(cities.get(0).District , "California");
+        assertEquals(cities.get(0).Population , 3694820);
+    }
+
+    @Test
+    void testCitiesInACountry()
+    {
+        ArrayList<City> cities = app.getAllCitiesInACountry("France");
+        assertEquals(cities.get(0).Name, "Paris");
+        assertEquals(cities.get(0).CountryCode, "FRA");
+        assertEquals(cities.get(0).District , "Île-de-France");
+        assertEquals(cities.get(0).Population , 2125246);
+    }
+
+
+    ///////////////////////////// CHANGE THESE TEST VALUES!!!!!!!!!!!!!!!!!!
+
+    @Test
+    void testNPopulatedCities()
+    {
+        ArrayList<City> cities = app.NPopulatedCities(4);
         assertEquals(cities.get(0).Name, "Los Angeles");
         assertEquals(cities.get(0).CountryCode, "USA");
         //assertEquals(cities.get(0).District , "California");
         assertEquals(cities.get(0).Population , 3694820);
+    }
+
+
+    ///////////////////////////// MAYBE???????????? CHANGE THESE TEST VALUES!!!!!!!!!!!!!!!!!!
+
+
+    @Test
+    void testNPopulatedCitiesInAContinent()
+    {
+        ArrayList<City> cities = app.getNCitiesInAContinent(3,"Europe");
+        assertEquals(cities.get(0).Name, "Moscow");
+        assertEquals(cities.get(0).CountryCode, "RUS");
+        assertEquals(cities.get(0).District , "Moscow (City)");
+        assertEquals(cities.get(0).Population , 8389200);
+    }
+
+
+
+
+    ///////////////////////////// CHANGE THESE TEST VALUES!!!!!!!!!!!!!!!!!!
+
+
+    @Test
+    void testNPopulatedCitiesInARegion()
+    {
+        ArrayList<City> cities = app.getNCitiesInARegion(4, "Caribbean");
+        assertEquals(cities.get(0).Name, "Moscow");
+        assertEquals(cities.get(0).CountryCode, "RUS");
+        assertEquals(cities.get(0).District , "Moscow (City)");
+        assertEquals(cities.get(0).Population , 8389200);
+    }
+
+    @Test
+    void testNPopulatedCitiesInADistrict()
+    {
+        ArrayList<City> cities = app.NPopulatedCitiesInADistrict(6, "California");
+        assertEquals(cities.get(0).Name, "Los Angeles");
+        assertEquals(cities.get(0).CountryCode, "USA");
+        //assertEquals(cities.get(0).District , "California");
+        assertEquals(cities.get(0).Population , 3694820);
+    }
+
+
+
+    ///////////////////////////// MAYBE???????????? CHANGE THESE TEST VALUES!!!!!!!!!!!!!!!!!!
+
+
+    @Test
+    void testNPopulatedCitiesInACountry()
+    {
+        ArrayList<City> cities = app.getNCitiesInACountry(3, "France");
+        assertEquals(cities.get(0).Name, "Paris");
+        assertEquals(cities.get(0).CountryCode, "FRA");
+        assertEquals(cities.get(0).District , "Île-de-France");
+        assertEquals(cities.get(0).Population , 2125246);
     }
 }
