a8b419e468f7ac1388c756286d77f3ab1bca5c90
Euan <40440665@live.napier.ac.uk>
2022-03-23 08:43:46 +0000

    Fixed Integration tests

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -19,28 +19,26 @@ public class AppIntegrationTest
 
 
 
-    ///////////////////////////// CHANGE THESE TEST VALUES!!!!!!!!!!!!!!!!!!
     @Test
     void testCitiesByPopulation()
     {
         ArrayList<City> cities = app.citiesByPopulation();
-        assertEquals(cities.get(0).Name, "Los Angeles");
-        assertEquals(cities.get(0).CountryCode, "USA");
-        assertEquals(cities.get(0).District , "California");
-        assertEquals(cities.get(0).Population , 3694820);
+        assertEquals(cities.get(0).Name, "Mumbai (Bombay)");
+        assertEquals(cities.get(0).CountryCode, "IND");
+        assertEquals(cities.get(0).District , "Maharashtra");
+        assertEquals(cities.get(0).Population , 10500000);
     }
 
 
 
-    ///////////////////////////// CHANGE THESE TEST VALUES!!!!!!!!!!!!!!!!!!
     @Test
     void testGetAllCitiesInAContinent()
     {
         ArrayList<City> cities = app.getAllCitiesInAContinent("North America");
-        assertEquals(cities.get(0).Name, "Los Angeles");
-        assertEquals(cities.get(0).CountryCode, "USA");
-        assertEquals(cities.get(0).District , "California");
-        assertEquals(cities.get(0).Population , 3694820);
+        assertEquals(cities.get(0).Name, "Ciudad de México");
+        assertEquals(cities.get(0).CountryCode, "MEX");
+        assertEquals(cities.get(0).District , "Distrito Federal");
+        assertEquals(cities.get(0).Population , 8591309);
     }
 
     @Test
@@ -49,23 +47,22 @@ public class AppIntegrationTest
         ArrayList<City> cities = app.citiesByDistrict("California");
         assertEquals(cities.get(0).Name, "Los Angeles");
         assertEquals(cities.get(0).CountryCode, "USA");
-        //assertEquals(cities.get(0).District , "California");
+        assertEquals(cities.get(0).District , "California");
         assertEquals(cities.get(0).Population , 3694820);
     }
 
 
 
 
-    ///////////////////////////// CHANGE THESE TEST VALUES!!!!!!!!!!!!!!!!!!
 
     @Test
     void testCitiesInARegion()
     {
         ArrayList<City> cities = app.getAllCitiesInARegion("Caribbean");
-        assertEquals(cities.get(0).Name, "Los Angeles");
-        assertEquals(cities.get(0).CountryCode, "USA");
-        assertEquals(cities.get(0).District , "California");
-        assertEquals(cities.get(0).Population , 3694820);
+        assertEquals(cities.get(0).Name, "La Habana");
+        assertEquals(cities.get(0).CountryCode, "CUB");
+        assertEquals(cities.get(0).District , "La Habana");
+        assertEquals(cities.get(0).Population , 2256000);
     }
 
     @Test
@@ -79,20 +76,18 @@ public class AppIntegrationTest
     }
 
 
-    ///////////////////////////// CHANGE THESE TEST VALUES!!!!!!!!!!!!!!!!!!
 
     @Test
     void testNPopulatedCities()
     {
         ArrayList<City> cities = app.NPopulatedCities(4);
-        assertEquals(cities.get(0).Name, "Los Angeles");
-        assertEquals(cities.get(0).CountryCode, "USA");
-        //assertEquals(cities.get(0).District , "California");
-        assertEquals(cities.get(0).Population , 3694820);
+        assertEquals(cities.get(0).Name, "Mumbai (Bombay)");
+        assertEquals(cities.get(0).CountryCode, "IND");
+        assertEquals(cities.get(0).District , "Maharashtra");
+        assertEquals(cities.get(0).Population , 10500000);
     }
 
 
-    ///////////////////////////// MAYBE???????????? CHANGE THESE TEST VALUES!!!!!!!!!!!!!!!!!!
 
 
     @Test
@@ -108,17 +103,16 @@ public class AppIntegrationTest
 
 
 
-    ///////////////////////////// CHANGE THESE TEST VALUES!!!!!!!!!!!!!!!!!!
 
 
     @Test
     void testNPopulatedCitiesInARegion()
     {
         ArrayList<City> cities = app.getNCitiesInARegion(4, "Caribbean");
-        assertEquals(cities.get(0).Name, "Moscow");
-        assertEquals(cities.get(0).CountryCode, "RUS");
-        assertEquals(cities.get(0).District , "Moscow (City)");
-        assertEquals(cities.get(0).Population , 8389200);
+        assertEquals(cities.get(0).Name, "La Habana");
+        assertEquals(cities.get(0).CountryCode, "CUB");
+        assertEquals(cities.get(0).District , "La Habana");
+        assertEquals(cities.get(0).Population , 2256000);
     }
 
     @Test
@@ -132,8 +126,6 @@ public class AppIntegrationTest
     }
 
 
-
-    ///////////////////////////// MAYBE???????????? CHANGE THESE TEST VALUES!!!!!!!!!!!!!!!!!!
 
 
     @Test
