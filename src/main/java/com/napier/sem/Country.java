280a953370c3a379bb65fcc7fc81aa1e1617214d
Euan <40440665@live.napier.ac.uk>
2022-04-04 13:30:55 +0100

    Added Country Class to feature/country-report branch

---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,21 @@
+package com.napier.sem;
+
+public class Country {
+    //Country Code
+    public String CountryCode;
+
+    //Country Name
+    public String Name;
+
+    //Continent
+    public String Continent;
+
+    //Region
+    public String Region;
+
+    //Population
+    public int Population;
+
+    //Capital
+    public String Capital;
+}
