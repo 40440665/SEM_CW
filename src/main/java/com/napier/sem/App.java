132aa7d08e4b1060221430b97e26ca8e53a978cd
Euan Walker <98456166+40440665@users.noreply.github.com>
2022-04-14 22:20:25 +0100

    Testing continent population

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -50,7 +50,7 @@ public class App {
         //Display top N populated cities in a country
         //ArrayList<City> cities = Test.nPopulatedCitiesInACountry(3, "France");
 
-        String populationOutput = Test.worldPopulation();
+        String populationOutput = Test.continentPopulation("Europe");
 
         System.out.println(populationOutput);
 
