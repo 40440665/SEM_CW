9374a957e26efd6a9851ea7d7a453144e5c2e157
Euan Walker <98456166+40440665@users.noreply.github.com>
2022-04-18 15:38:48 +0100

    Added Region Population sql

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -695,10 +695,13 @@ public class App {
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT SUM(population) AS Population "
-                            + "FROM country "
-                            + " WHERE Region = '" + region + "' ";
-
+                    "SELECT country.Region AS Name, SUM(country.Population) AS Population, "
+                        + " (SELECT SUM(city.Population) FROM city, country WHERE city.CountryCode = country.Code AND country.Region = 'Caribbean') AS 'Population Living in Cities', "
+                        + " ROUND(((SELECT SUM(city.Population) FROM city, country WHERE city.CountryCode = country.Code AND country.Region = 'Caribbean') / (SUM(country.Population)))*100,1) AS 'Percentage of Population Living in Cities', "
+                        + " (SUM(country.Population) - (SELECT SUM(city.Population) FROM city, country WHERE city.CountryCode = country.Code AND Region = 'Caribbean')) AS 'Population not Living in cities', "
+                        + " ROUND((((SUM(country.Population)) - (SELECT SUM(city.Population) FROM city, country WHERE city.CountryCode = country.Code AND country.Region = 'Caribbean')) / SUM(country.Population))*100, 1) AS 'Percentage of Population not Living in Cities' "
+                        + " FROM country WHERE Region = 'Caribbean' ";
+                    
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Extract population information
